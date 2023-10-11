82053f8b3a4cad065093f84bda79e2b5b3655086
GVVGhost <gvv.ghost@gmail.com>
2023-10-11 11:41:11 +0300

    APK update module added and configured

--- testMobile/App.jsx
+++ testMobile/App.jsx
@@ -1,13 +1,100 @@
-import React from 'react';
+//@flow
+import {Component} from 'react';
 import {AuthProvider} from 'utils/AuthContext';
 import {Navigator} from 'navigation/Navigator';
+import {Alert} from 'react-native';
+import * as UpdateAPK from 'rn-update-apk';
 
-function App() {
-  return (
-    <AuthProvider>
-      <Navigator />
-    </AuthProvider>
-  );
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onDownloadProgress: -1,
+      allApp: [],
+      allNonSystemsApp: [],
+      userName: '',
+    };
+
+    // eslint-disable-next-line no-undef
+    updater = new UpdateAPK.UpdateAPK({
+      apkVersionUrl: '',
+      apkVersionOptions: {
+        method: 'GET',
+        headers: {},
+      },
+      apkOptions: {
+        headers: {},
+      },
+      fileProviderAuthority: 'com.example.provider',
+      needUpdateApp: performUpdate => {
+        Alert.alert(
+          'Update Available',
+          'New version released, do you want to update? ' +
+            [
+              {
+                text: 'Cancel',
+                onPress: () => {},
+              },
+              {text: 'Update', onPress: () => performUpdate(true)},
+            ],
+        );
+      },
+      forceUpdateApp: () => {
+        console.log('forceUpdateApp callback called');
+      },
+
+      notNeedUpdateApp: () => {
+        console.log('notNeedUpdateApp callback called');
+      },
+
+      downloadApkStart: () => {
+        console.log('downloadApkStart callback called');
+      },
+
+      downloadApkProgress: progress => {
+        console.log(`downloadApkProgress callback called - ${progress}%...`);
+        this.setState({downloadProgress: progress});
+      },
+
+      downloadApkEnd: () => {
+        console.log('downloadApkEnd callback called');
+      },
+
+      onError: err => {
+        console.log('onError callback called', err);
+        Alert.alert('There was an error', err.message);
+      },
+    });
+  }
+
+  async componentDidMount() {
+    UpdateAPK.getApps()
+      .then(apps => {
+        console.log('Installed Apps: ', JSON.stringify(apps));
+        this.setState({allApps: apps});
+      })
+      .catch(e => console.log('Unable to getApps?', e));
+
+    UpdateAPK.getNonSystemApps()
+      .then(apps => {
+        console.log('Installed Non-System Apps: ', JSON.stringify(apps));
+        this.setState({allNonSystemApps: apps});
+      })
+      .catch(e => console.log('Unable to getNonSystemApps?', e));
+  }
+
+  _onCheckServerVersion = () => {
+    console.log('checking for update');
+    // eslint-disable-next-line no-undef
+    updater.checkUpdate();
+  };
+
+  render() {
+    return (
+      <AuthProvider>
+        <Navigator />
+      </AuthProvider>
+    );
+  }
 }
-
-export default App;
